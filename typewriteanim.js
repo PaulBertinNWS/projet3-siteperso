75e94a34224a5a286ec5a93e9e9ab8189cdae350
PaulBertinNWS <56394502+PaulBertinNWS@users.noreply.github.com>
2020-03-10 16:55:45 +0100

    ajout services et portfolio

--- typewriteanim.js
+++ typewriteanim.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded',function(event){
       // text finished, call callback if there is a callback function
       else if (typeof fnCallback == 'function') {
         // call callback after timeout
-        setTimeout(fnCallback, 2500);
+        setTimeout(fnCallback, 1500);
       }
     }
     // start a typewriter animation for a text in the dataText array
